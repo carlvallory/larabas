56591c2890bd2177af81006358b558989e27ef29
aquilesdoldan <67962505+aquilesdoldan@users.noreply.github.com>
2022-11-22 02:27:59 -0300

    About

---
+++ resources/views/about.blade.php
@@ -0,0 +1,232 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="apple-touch-icon" sizes="57x57" href="img/favicon/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="img/favicon/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="img/favicon/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="img/favicon/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="img/favicon/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="img/favicon/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192" href="img/favicon/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="img/favicon/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png">
+  <link rel="manifest" href="img/favicon/manifest.json">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="img/favicon/ms-icon-144x144.png">
+  <meta name="theme-color" content="#ffffff">
+  <title>Abas Inmobiliaria</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+  <!--Font Awesome-->
+  <script src="https://kit.fontawesome.com/192ee6ec02.js" crossorigin="anonymous"></script>
+
+  <!--Google font-->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;800&family=Roboto+Slab:wght@300;500&display=swap" rel="stylesheet">
+
+  <!--Css Abas-->
+  <link href="css/abas.css" rel="stylesheet">
+
+</head>
+
+<body style="overflow-x: hidden;">
+
+ <!--Cabecera-->
+ <div class="container">
+  <nav class="navbar navbar-expand-lg border-bottom">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"><img src="img/logo.png" height="60px"></a>
+      <button class="navbar-toggler menu-res" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span><img src="img/burger.svg"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+         <!--ul vacío-->
+        </ul>
+        <!--menu-->
+        <span class="navbar-text">
+          <ul class="navbar-nav nav-res me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Quienes somos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Loteamientos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Ofrecenos tu propiedad</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link boton-verde btn-res" href="#">Contacto</a>
+            </li>
+          </ul>
+        </span>
+      </div>
+    </div>
+  </nav>
+ </div>
+ 
+<!--contenido about-->
+
+<div class="container">
+
+  <!--portada-->
+  <div class="portada-about overflow-hidden" style="background-image: url(img/nosotros.jpg);">
+    <div class="cover-portada">
+      <h2 class="text-white w-100 text-center">Quienes somos</h2>
+      <p class="f-2 text-white fs-bolt w-75 mx-auto text-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+    </div>  
+  </div>
+
+  <!--Texto primario-->
+
+  <p class="fs-5 mt-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+  <!--texto secundario-->
+  <p class="fs-6 mt-5 text-secondary">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+  <!--imagen triple-->
+
+  <div class="row mt-5">
+    <div class="col-md-4 col-3">
+      <img src="img/nosotros.jpg" width="100%" class="rounded-4">
+    </div>
+    <div class="col-md-4 col-3">
+      <img src="img/nosotros.jpg" width="100%" class="rounded-4">
+    </div>
+    <div class="col-md-4 col-3">
+      <img src="img/nosotros.jpg" width="100%" class="rounded-4">
+    </div>
+  </div>
+
+  <!--texto secundario-->
+  <p class="fs-6 mt-5 text-secondary">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+  <!--imagen doble-->
+
+  <div class="row mt-5">
+    <div class="col-md-6 col-3">
+      <img src="img/nosotros.jpg" width="100%" class="rounded-4">
+    </div>
+    <div class="col-md-6 col-3">
+      <img src="img/nosotros.jpg" width="100%" class="rounded-4">
+    </div>
+  </div>
+
+  <!--texto secundario-->
+  <p class="fs-6 mt-5 text-secondary">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+  <!--imagen simple-->
+
+  <div class="row mt-5">
+    <div class="col-md col">
+      <img src="img/nosotros.jpg" width="100%" class="rounded-4">
+    </div>
+  </div>
+
+</div>
+
+<!--fin contenido about-->
+
+<!--espaciador-->
+<div class="w-100 py-5"></div>
+
+<!--footer-->
+<div class="w-100 bg-gris-claro text-gris-oscuro">
+
+  <div class="container">
+    <div class="row pt-5 mb-4">
+      <div class="col-md">
+        <img src="img/logo-abas-gris.png" class="logo-footer">
+
+        <ul>
+          <ul class="nav flex-column">
+            <li class="nav-item">
+              <a class="nav-link text-gris-oscuro" href="#"><img src="img/mail.svg"> hola@abas.com.py </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-gris-oscuro" href="#"><img src="img/phone.svg"> +595 - 21 234 725</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-gris-oscuro" href="#"><img src="img/point.svg"> Avda. Mcal. Lopez 2030</a>
+            </li>
+          </ul>
+        </ul>
+      </div>
+
+      <div class="col-md borde-footer">
+        <ul>
+          <ul class="nav flex-column">
+            <li class="nav-item">
+              <a class="nav-link text-gris-oscuro" href="#">Promociones especiales</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-gris-oscuro" href="#">Proyectos vigentes</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-gris-oscuro" href="#">Responsabilidad Social</a>
+            </li>
+          </ul>
+        </ul>
+        
+      </div>
+      <div class="col-md borde-footer">
+        <ul>
+          <ul class="nav flex-column">
+            <li class="nav-item">
+              <a class="nav-link text-gris-oscuro" href="#">Quienes somos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-gris-oscuro" href="#">Proyectos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-gris-oscuro" href="#">Blog</a>
+            </li>
+          </ul>
+        </ul>
+      </div>
+      <div class="col-md borde-footer">
+        <ul>
+          <ul class="nav flex-column">
+            <li class="nav-item">
+              <a class="nav-link text-gris-oscuro" href="#">Contacto</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-gris-oscuro" href="#">Sucursales</a>
+            </li>
+          </ul>
+        </ul>
+        <div class="w-100 mt-3 d-block ms-5">
+          <a href="#" class="social"><img src="img/fb.svg"></a>
+          <a href="#" class="social"><img src="img/tw.svg"></a>
+          <a href="#" class="social"><img src="img/ig.svg"></a>
+          <a href="#" class="social"><img src="img/yt.svg"></a>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <div class="w-100 bg-gris-oscuro text-center py-4">
+    Copyright © 2019. Todos los derechos reservados.
+  </div>
+</div>
+  <script src="js/jquery-3.6.1.min.js"></script>
+  <!--Bootstrap js-->
+  <script src="js/bootstrap.bundle.min.js"
+    integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
+    crossorigin="anonymous"></script>
+  <script src="js/scripts.js"></script>
+</body>
+
+</html>
