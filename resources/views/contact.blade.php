505bd1c0bd894e4ed4a0ad3be374f0bda3230230
aquilesdoldan <67962505+aquilesdoldan@users.noreply.github.com>
2022-11-24 19:42:58 -0300

    Formulario de contacto

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,7 +1,65 @@
 @extends('layouts.frontend')
 @section('content')
  
+<!--contacto-->
 
+<div class="container mt-5">
+
+<h3 class="text-azul">Contacto</h3>
+
+<p class="fs-5 text-secondary">Dejanos tus consultas o sugerencias, nuestro equipo se comunicará contigo en la brevedad posible</p>
+
+<!--formulario-->
+<form class="row g-3 mt-5">
+  <div class="col-md-6">
+    <label class="form-label">Nombre</label>
+    <input type="text" class="form-control" id="inputEmail4">
+  </div>
+  <div class="col-md-6">
+    <label for="inputPassword4" class="form-label">Apellido</label>
+    <input type="text" class="form-control" id="inputPassword4">
+  </div>
+  <div class="col-12">
+    <label for="inputAddress" class="form-label">Teléfono o celular</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="098 ..">
+  </div>
+  <div class="col-12">
+    <label for="inputAddress2" class="form-label">Correo electróico</label>
+    <input type="email" class="form-control" id="inputAddress2" placeholder="usuario@email.com">
+  </div>
+  <div class="col-md-6">
+    <label for="inputCity" class="form-label">Ciudad</label>
+    <input type="text" class="form-control" id="inputCity">
+  </div>
+  <div class="col-md-6">
+    <label for="inputState" class="form-label">Asunto</label>
+    <select id="inputState" class="form-select">
+      <option selected>Seleccionar...</option>
+      <option>...</option>
+    </select>
+  </div>
+
+  <div class="col-12">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gridCheck">
+      <label class="form-check-label" for="gridCheck">
+        Quiero recibir ofertas y novedades
+      </label>
+    </div>
+  </div>
+
+  <di class="col-12">
+    <div class="mb-3">
+      <label for="exampleFormControlTextarea1" class="form-label">Consulta o sugerencia</label>
+      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
+  </di>
+  <div class="col-12">
+    <button type="submit" class="btn bg-verde">Enviar mensaje</button>
+  </div>
+</form>
+
+<!--fin formulario-->
 
 
 <!--espaciador-->
